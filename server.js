7d2605a60681a857994e93cb99ffd53f87bc9476
rluick15 <rluick15@gmail.com>
2016-05-11 18:58:05 -0400

    Setup midleware

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 'use strict';
 const express = require('express');
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+let helloMiddleware = (req, res, next) => {
+	req.hello = "Hello! It's me";
+	next();
+}
+
+app.use('/dashboard', helloMiddleware);
+
+app.get('/', (req, res, next) => {
+	res.send('<h1>Hello Express!</h1>');
+});
+
+app.get('/dashboard', (req, res, next) => {
+	res.send('<h1>This is the dashboard page! Middleware says: ' + req.hello + '</h1>');
+});
+
+app.listen(app.get('port'), () => {
+	console.log("ChatCat Running on Port: ", app.get('port'));
+});

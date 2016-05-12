4132f924bf463dde638863bcef603d7e6955cb2c
rluick15 <rluick15@gmail.com>
2016-05-11 20:21:31 -0400

    setup static fields with ejs

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 'use strict';
 const express = require('express');
 const app = express();
+
 app.set('port', process.env.PORT || 3000);
-
-let helloMiddleware = (req, res, next) => {
-	req.hello = "Hello! It's me";
-	next();
-}
-
-app.use('/dashboard', helloMiddleware);
+	app.use(express.static('views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res, next) => {
-	res.send('<h1>Hello Express!</h1>');
-});
-
-app.get('/dashboard', (req, res, next) => {
-	res.send('<h1>This is the dashboard page! Middleware says: ' + req.hello + '</h1>');
+	res.render('login');
 });
 
 app.listen(app.get('port'), () => {
